dd420ea23bf848c6c2c222a458fb59ec18cac5f2
abbas501 <syedd.abbasali7@gmail.com>
2016-10-06 17:17:59 +0530

    Add files via upload

---
+++ Array1Ex.java
@@ -0,0 +1,21 @@
+public class Array1Ex {
+public static void main(String[] args) {
+// int a[]; //Array Declaration
+ int a[] = new int [2]; //Array Initialization 
+ //int []b;  // Array Declaration
+ int[] b = new int[3];
+ //int[] c;  // Array Declaration
+   int c[] = new  int[4];
+   
+  a[0]=1;
+  a[1]=2;
+  //a[2]=4;
+  System.out.println(a[0]);
+   
+   
+// int [6] d; //Array Declaration Error can't initialized array @ the time of Declaration.
+   	
+ 
+  
+}
+}
